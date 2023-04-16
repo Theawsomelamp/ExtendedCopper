432cc7524553ae5696f2e29623d6dc315b04c59f
sebas <sebasrobers@gmail.com>
2023-04-16 15:45:40 +0200

    Added oxidizing copper pressure plates

--- src/main/java/com/lankaster/extendedcopper/block/custom/ModPressurePlateBlock.java
+++ src/main/java/com/lankaster/extendedcopper/block/custom/ModPressurePlateBlock.java
@@ -1,38 +1,46 @@
 package com.lankaster.extendedcopper.block.custom;
 
-import net.minecraft.block.PressurePlateBlock;
-import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Oxidizable;
+import net.minecraft.block.WeightedPressurePlateBlock;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
-import java.util.Iterator;
-import java.util.List;
+public class ModPressurePlateBlock extends WeightedPressurePlateBlock implements Oxidizable {
+    private final Oxidizable.OxidationLevel oxidationLevel;
+    private final int weight;
 
-
-public class ModPressurePlateBlock extends PressurePlateBlock {
-    public ModPressurePlateBlock(ActivationRule type, Settings settings) {
-        super(type, settings, SoundEvents.BLOCK_METAL_PRESSURE_PLATE_CLICK_OFF, SoundEvents.BLOCK_METAL_PRESSURE_PLATE_CLICK_ON);
+    public ModPressurePlateBlock(Oxidizable.OxidationLevel oxidationLevel, int weight, Settings settings) {
+        super(weight, settings, SoundEvents.BLOCK_METAL_PRESSURE_PLATE_CLICK_OFF, SoundEvents.BLOCK_METAL_PRESSURE_PLATE_CLICK_ON);
+        this.oxidationLevel = oxidationLevel;
+        this.weight = weight;
     }
 
     @Override
     protected int getRedstoneOutput(World world, BlockPos pos) {
-        Box box = BOX.offset(pos);
-        List list3;
-        list3 = world.getNonSpectatingEntities(ItemEntity.class, box);
-
-        if (!list3.isEmpty()) {
-
-            for (Object o : list3) {
-                Entity entity = (Entity) o;
-                if (!entity.canAvoidTraps()) {
-                    return 15;
-                }
-            }
+        int i = Math.min(world.getNonSpectatingEntities(ItemEntity.class, BOX.offset(pos)).size(), this.weight);
+        if (i > 0) {
+            float f = (float)Math.min(this.weight, i) / (float)this.weight;
+            return MathHelper.ceil(f * 15.0F);
+        } else {
+            return 0;
         }
+    }
 
-        return 0;
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        this.tickDegradation(state, world, pos, random);
+    }
+
+    public boolean hasRandomTicks(BlockState state) {
+        return Oxidizable.getIncreasedOxidationBlock(state.getBlock()).isPresent();
+    }
+
+    public Oxidizable.OxidationLevel getDegradationLevel() {
+        return this.oxidationLevel;
     }
 }
